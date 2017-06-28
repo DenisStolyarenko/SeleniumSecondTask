6838d429ad1259e2102189ae8e5c2c8bb4ea97d6
Denis Stolyarenko <denis_stolyarenko@epam.com>
2017-06-28 11:05:14 +0600

    add 2 classes

---
+++ src/main/java/ctc/pages/MyAttachmentsPage.java
@@ -0,0 +1,10 @@
+package ctc.pages;
+
+
+import org.openqa.selenium.WebDriver;
+
+public class MyAttachmentsPage extends AbstractPage {
+    public MyAttachmentsPage(WebDriver driver) {
+        super(driver);
+    }
+}
